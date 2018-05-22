6124fbecb126479a414d20e3f3de9648f9823fbc
Vincent <vinh.nglx@gmail.com>
2018-05-22 17:35:18 +0800

    add get("/*path", PageController, :index) to handle all urls will be rendered on PageController#index

--- lib/graphql_example_web/router.ex
+++ lib/graphql_example_web/router.ex
@@ -2,21 +2,23 @@ defmodule GraphqlExampleWeb.Router do
   use GraphqlExampleWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", GraphqlExampleWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+    get("/*path", PageController, :index)
   end
 
   # Other scopes may use custom stacks.
