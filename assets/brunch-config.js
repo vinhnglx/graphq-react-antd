17de2d3c1b008de17787eb9c8f5a2d7de7c420ba
Vincent <vinh.nglx@gmail.com>
2018-05-22 17:28:18 +0800

    Update brunch.config

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -2,14 +2,17 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
+      // joinTo: 'js/app.js',
+      entryPoints: {
+        'js/app.js': 'js/app.js',
+      },
 
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
       // joinTo: {
-      //   "js/app.js": /^js/,
-      //   "js/vendor.js": /^(?!js)/
-      // }
+      //   'js/app.js': 'js/app.js',
+      //   'js/admin.js': 'js/admin.js',
+      // },
       //
       // To change the order of concatenation of files, explicitly mention here
       // order: {
@@ -20,43 +23,50 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: "css/app.css"
+      joinTo: 'css/app.css',
     },
-    templates: {
-      joinTo: "js/app.js"
-    }
+    // templates: {
+    //   joinTo: 'js/app.js'
+    // }
   },
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
     // By default, we set this to "/assets/static". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(static)/
+    assets: /^(static)/,
   },
 
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor"],
+    watched: ['static', 'css', 'js', 'vendor'],
     // Where to compile files to
-    public: "../priv/static"
+    public: '../priv/static',
   },
 
   // Configure your plugins
   plugins: {
     babel: {
       // Do not use ES6 compiler in vendor code
-      ignore: [/vendor/]
-    }
+      presets: ['es2015', 'react', 'stage-1'],
+      ignore: [/vendor/],
+    },
+    sass: {
+      options: {
+        includePaths: ['node_modules/antd/dist'], // tell sass-brunch where to look for files to @import
+        precision: 8, // minimum precision required by bootstrap
+      },
+    },
   },
 
   modules: {
     autoRequire: {
-      "js/app.js": ["js/app"]
-    }
+      'js/app.js': ['js/app'],
+    },
   },
 
   npm: {
-    enabled: true
-  }
+    enabled: true,
+  },
 };
